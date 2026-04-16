091569a9251eeb1d14a04489b8c93f36651e0343
C2300040802 <C2300040802@C2300040802>
2026-04-16 13:15:25 +0800

    feat:读取交互提交

--- data-monitor/SL.MLineDataPrecisionTracking.Core/Services/ProductionLineDataCollectionService.cs
+++ data-monitor/SL.MLineDataPrecisionTracking.Core/Services/ProductionLineDataCollectionService.cs
@@ -1,15 +1,16 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using SL.MLineDataPrecisionTracking.Infrastructure.Common;
 using SL.MLineDataPrecisionTracking.Infrastructure.PLCCommunication;
 using SL.MLineDataPrecisionTracking.Infrastructure.Storage;
 using SL.MLineDataPrecisionTracking.Models.Dtos;
 using SL.MLineDataPrecisionTracking.Models.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+using SqlSugar.Extensions;
 
 namespace SL.MLineDataPrecisionTracking.Core.Services
 {
@@ -19,33 +20,86 @@ namespace SL.MLineDataPrecisionTracking.Core.Services
 
         List<DevPlcPointMcDto> _lineReadPlcInfo;
         McpCommunication _mcpCommunication;
-        public ProductionLineDataCollectionService(Tb_EquipmentRepository equipmentRepository, McpCommunication  mcpCommunication)
+
+        int _plcJump;
+        DevPlcPointMcDto _plcJumpPoint;
+        DevPlcPointMcDto _pcJumpPoint;
+        int _pcJump;
+
+        public ProductionLineDataCollectionService(
+            Tb_EquipmentRepository equipmentRepository,
+            McpCommunication mcpCommunication
+        )
         {
             _equipmentRepository = equipmentRepository;
-            _mcpCommunication= mcpCommunication;
+            _mcpCommunication = mcpCommunication;
         }
 
-        public  async Task ExecuteAsync(CancellationToken stoppingToken)
+        public async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (stoppingToken.IsCancellationRequested)
             {
                 try
                 {
                     await InitPlcAddre();
-                  //var readValue=   await _mcpCommunication.ReadAsync(_lineReadPlcInfo);
-                   
 
+                    while (true)
+                    {
+                        try
+                        {
+                            if (await CanCollection() is false)
+                            {
+                                continue;
+                            }
+                            CollectionData();
+                            CallPlcCollectionOK();
+                        }
+                        catch (Exception ex)
+                        {
+                            throw;
+                        }
+                        finally
+                        {
+                            await Task.Delay(1000);
+                        }
+                    }
                 }
                 catch (Exception ex)
                 {
-
                     throw;
                 }
-                finally { await Task.Delay(1000); }
-            
-
-              
+                finally
+                {
+                    await Task.Delay(1000);
+                }
             }
+        }
+
+        private void CollectionData() { }
+
+        private async Task CallPlcCollectionOK()
+        { // 将读取的plc的 跳变回写
+            _pcJumpPoint.Value = new List<object> { _pcJump };
+            if ((await _mcpCommunication.WriteAsync(_pcJumpPoint)).IsSuccess)
+            { //写成功后更新自己的的跳变
+                _plcJump = _plcJump;
+            }
+        }
+
+        private async Task<bool> CanCollection()
+        {
+            var plcJumpValue = await _mcpCommunication.ReadAsync(_plcJumpPoint);
+            if (plcJumpValue.IsSuccess is false)
+            {
+                return false;
+            }
+            var readPlcJump = plcJumpValue.Data.Value[0].ObjToInt();
+            if (readPlcJump == 0 || readPlcJump == _plcJump)
+            {
+                return false;
+            }
+            _plcJump = readPlcJump;
+            return true;
         }
 
         private async Task InitPlcAddre()
@@ -53,18 +107,25 @@ namespace SL.MLineDataPrecisionTracking.Core.Services
             _lineReadPlcInfo = new List<DevPlcPointMcDto>();
             foreach (var devInfo in await _equipmentRepository.GetEquipmentAllAsync())
             {
-                //var devName = devInfo.DeviceName;
                 foreach (var plcLinkeInfo in devInfo.PlcConnections)
                 {
-                    //var plcIp = plcLinkeInfo.IpAddress;
-                    //var plcPort=plcLinkeInfo.Port;
                     foreach (var plcAddres in plcLinkeInfo.Points)
                     {
-                        _lineReadPlcInfo.Add(new DevPlcPointMcDto(devInfo.DeviceName, plcAddres.PointName,plcLinkeInfo.IpAddress, plcLinkeInfo.Port,plcAddres.Area.ToPrefix(),plcAddres.DataType.ToTypeCode(),plcAddres.Address,plcAddres.Length));
+                        _lineReadPlcInfo.Add(
+                            new DevPlcPointMcDto(
+                                devInfo.DeviceName,
+                                plcAddres.PointName,
+                                plcLinkeInfo.IpAddress,
+                                plcLinkeInfo.Port,
+                                plcAddres.Area.ToPrefix(),
+                                plcAddres.DataType.ToTypeCode(),
+                                plcAddres.Address,
+                                plcAddres.Length
+                            )
+                        );
                     }
                 }
             }
-         
         }
     }
 }
