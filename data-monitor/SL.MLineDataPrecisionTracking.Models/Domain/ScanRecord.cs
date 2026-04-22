e752a12bd3b5fef7613963e113045f883013d1fc
C2300040802 <C2300040802@C2300040802>
2026-04-22 13:09:14 +0800

    fix：修复查询类型

--- data-monitor/SL.MLineDataPrecisionTracking.Models/Domain/ScanRecord.cs
+++ data-monitor/SL.MLineDataPrecisionTracking.Models/Domain/ScanRecord.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace SL.MLineDataPrecisionTracking.Models.Domain
 {
-    public class ScanRecord
+    public class ScanRecord : ObservableObject
     {
         public string MarkingNo { get; set; }
-        public bool IsHave { get; set; }
+
+        private bool _isHave;
+        public bool IsHave
+        {
+            get => _isHave;
+            set => SetProperty(ref _isHave, value);
+        }
     }
 }
