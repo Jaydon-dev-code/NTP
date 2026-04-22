b01a3fd8be389c29c7c37ae901475dabe9cca822
C2300040802 <C2300040802@C2300040802>
2026-04-22 13:11:34 +0800

    feat:添加转换器

---
+++ data-monitor/SL.MLineDataPrecisionTracking.Client/Common/EnumToStringtConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace SL.MLineDataPrecisionTracking.Client.Common
+{
+    public class EnumToStringtConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (value as Enum)?.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
