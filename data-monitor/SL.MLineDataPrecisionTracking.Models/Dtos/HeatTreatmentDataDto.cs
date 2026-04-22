279f2ecfa422f980355d4e80de23fbc886d5d638
C2300040802 <C2300040802@C2300040802>
2026-04-23 06:21:57 +0800

    fix:数据类型修改为string

--- data-monitor/SL.MLineDataPrecisionTracking.Models/Dtos/HeatTreatmentDataDto.cs
+++ data-monitor/SL.MLineDataPrecisionTracking.Models/Dtos/HeatTreatmentDataDto.cs
@@ -18,68 +18,71 @@ namespace SL.MLineDataPrecisionTracking.Models.Dtos
             // 自动映射所有同名属性
             AutoMapProperties(tb_LineSummary, this);
         }
+        [Description("时间")]
+        public DateTime RecordTime { get; set; } = DateTime.Now;
 
         /// <summary>
         /// 加热时间（单位：秒/分钟，根据业务定义）
         /// </summary>
-        [Description("加热时间")]
-        public double HeatingTime { get; set; }
+        [Description("加热时间（秒）")]
+        public string HeatingTime { get; set; }
 
         /// <summary>
         /// 冷却开始时间（yyyy-MM-dd HH:mm:ss 格式）
         /// </summary>
-        [Description("冷却时间")]
-        public DateTime CoolingStartTime { get; set; }
+        [Description("冷却时间（秒）")]
+        public string CoolingStartTime { get; set; }
 
         /// <summary>
         /// 冷却时间（单位：秒/分钟，根据业务定义）
         /// </summary>
-        [Description("冷却时间")]
-        public double CoolingTime { get; set; }
+        [Description("冷却时间（秒）")]
+        public string CoolingTime { get; set; }
 
         /// <summary>
         /// 输出功率（单位：kW/W）
         /// </summary>
-        [Description("输出功率")]
+        [Description("输出功率（kw）")]
         public string OutputPower { get; set; }
 
         /// <summary>
         /// 输出电压（单位：V）
         /// </summary>
-        [Description("输出电压")]
+        [Description("输出电压（V）")]
         public string OutputVoltage { get; set; }
 
         /// <summary>
         /// 输出频率（单位：Hz）
         /// </summary>
-        [Description("输出频率")]
+        [Description("输出频率（kHz）")]
         public string OutputFrequency { get; set; }
 
         /// <summary>
         /// 外喷冷却水量
         /// </summary>
-        [Description("外喷冷却水量")]
+        [Description("外喷冷却水量（L/分）")]
         public string ExternalSprayCoolingWater { get; set; }
 
         /// <summary>
         /// 辅喷冷却水量
         /// </summary>
-        [Description("辅喷冷却水量")]
+        [Description("辅喷冷却水量（L/分）")]
         public string AuxiliarySprayCoolingWater { get; set; }
 
         /// <summary>
         /// 内喷冷却水量
         /// </summary>
-        [Description("内喷冷却水量")]
+        [Description("内喷冷却水量（L/分）")]
         public string InternalSprayCoolingWater { get; set; }
 
         /// <summary>
         /// 二维码打标内容
         /// </summary>
-        [Description("二维码打标内容")]
+        [Description("序列码")]
         public string MarkingNo { get; set; }
 
         private bool _isHave = true;
+        [Description("是否存在记录")]
         public bool IsHave
         {
             get => _isHave;
