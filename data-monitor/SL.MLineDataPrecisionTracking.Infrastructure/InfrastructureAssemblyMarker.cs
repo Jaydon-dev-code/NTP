f4d2875def3e5862cfbd0f563fd29b351afe0142
C2300040802 <C2300040802@C2300040802>
2026-04-28 11:03:51 +0800

    feat：添加反射标识类

---
+++ data-monitor/SL.MLineDataPrecisionTracking.Infrastructure/InfrastructureAssemblyMarker.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SL.MLineDataPrecisionTracking.Infrastructure
+{
+    // 空类，仅用于标记程序集
+    public class InfrastructureAssemblyMarker { }
+}
