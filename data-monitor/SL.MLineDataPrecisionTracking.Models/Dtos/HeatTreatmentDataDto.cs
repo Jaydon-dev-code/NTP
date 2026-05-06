835f79ba5c3d378e2da2ca80a94f0388f7b4ea97
C2300040802 <C2300040802@C2300040802>
2026-05-06 15:51:27 +0800

    fieat:添加抬头描述

--- data-monitor/SL.MLineDataPrecisionTracking.Models/Dtos/HeatTreatmentDataDto.cs
+++ data-monitor/SL.MLineDataPrecisionTracking.Models/Dtos/HeatTreatmentDataDto.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
 using SL.MLineDataPrecisionTracking.Models.Entities;
 
 namespace SL.MLineDataPrecisionTracking.Models.Dtos
@@ -10,7 +12,7 @@ namespace SL.MLineDataPrecisionTracking.Models.Dtos
     /// <summary>
     /// 热处理数据DTO
     /// </summary>
-    public class HeatTreatmentDataDto
+    public class HeatTreatmentDataDto : ObservableObject
     {
         /// <summary>
         /// 构造函数
@@ -50,56 +52,70 @@ namespace SL.MLineDataPrecisionTracking.Models.Dtos
         /// <summary>
         /// 记录时间
         /// </summary>
+        [Description("记录时间")]
         public DateTime RecordTime { get; set; }
+
+        /// <summary>
+        /// 二维码打标内容
+        /// </summary>
+        [Description("序列码")]
+        public string MarkingNo { get; set; }
 
         /// <summary>
         /// 加热时间
         /// </summary>
+        [Description("加热时间（秒）")]
         public string HeatingTime { get; set; }
 
         /// <summary>
         /// 冷却开始时间
         /// </summary>
+        [Description("冷却开始时间（秒）")]
         public string CoolingStartTime { get; set; }
 
         /// <summary>
         /// 冷却时间
         /// </summary>
+        [Description("冷却时间（秒）")]
         public string CoolingTime { get; set; }
 
         /// <summary>
         /// 输出功率
         /// </summary>
+        [Description("输出功率（kw）")]
         public string OutputPower { get; set; }
 
         /// <summary>
         /// 输出电压
         /// </summary>
+        [Description("输出电压（V）")]
         public string OutputVoltage { get; set; }
 
         /// <summary>
         /// 输出频率
         /// </summary>
+        [Description("输出频率（kHz）")]
         public string OutputFrequency { get; set; }
 
         /// <summary>
         /// 外喷冷却水量
         /// </summary>
+        [Description("外喷冷却水量（L/分）")]
         public string ExternalSprayCoolingWater { get; set; }
 
         /// <summary>
         /// 辅喷冷却水量
         /// </summary>
+        [Description("辅喷冷却水量（L/分）")]
         public string AuxiliarySprayCoolingWater { get; set; }
 
-        /// <summary>
-        /// 二维码打标内容
-        /// </summary>
-        public string MarkingNo { get; set; }
+        private bool _isHave = true;
 
-        /// <summary>
-        /// 是否存在
-        /// </summary>
-        public bool IsHave { get; set; }
+        [Description("是否存在记录")]
+        public bool IsHave
+        {
+            get => _isHave;
+            set => SetProperty(ref _isHave, value);
+        }
     }
 }
