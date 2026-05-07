a476f8e7cbf34e8abb62aa4f26b6aabb288195f2
C2300040802 <C2300040802@C2300040802>
2026-05-07 11:19:03 +0800

    feat:优化安装脚本

--- data-monitor/SL.MLineDataPrecisionTracking.Service/InstallInfo/InstallUtil.bat
+++ data-monitor/SL.MLineDataPrecisionTracking.Service/InstallInfo/InstallUtil.bat
@@ -8,5 +8,15 @@ echo ==============================
 set "SERVICE_EXE=%~dp0SL.MLineDataPrecisionTracking.Service.exe"
 %windir%\Microsoft.NET\Framework64\v4.0.30319\InstallUtil.exe "%SERVICE_EXE%"
 echo.
-echo 安装完成！
+echo ==============================
+echo  安装完成，正在启动服务...
+echo ==============================
+:: 【关键】你的服务名称（必须和 ProjectInstaller 里的 ServiceName 一样！）
+set "SERVICE_NAME=SL.MLineDataPrecisionTracking.Service"
+
+:: 自动启动服务
+net start "%SERVICE_NAME%"
+
+echo.
+echo 服务启动成功！
 pause
