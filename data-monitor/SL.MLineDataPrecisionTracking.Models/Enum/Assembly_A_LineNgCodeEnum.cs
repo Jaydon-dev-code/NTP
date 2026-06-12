a1ff64f54c62e406d726683f3387d415a574e115
C2300040802 <C2300040802@C2300040802>
2026-06-12 11:50:11 +0800

    feat:添加a线对应的ng代码枚举和屏蔽工位枚举

---
+++ data-monitor/SL.MLineDataPrecisionTracking.Models/Enum/Assembly_A_LineNgCodeEnum.cs
@@ -0,0 +1,47 @@
+﻿using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.Remoting.Lifetime;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SL.MLineDataPrecisionTracking.Models.Enum
+{
+    /// <summary>
+    /// 6-3装配线NG代码枚举 采集的数字 代表对应的NG
+    /// </summary>
+    public enum Assembly_A_LineNgCodeEnum
+    {
+        None = 0,
+        [Description("460 HUB侧 BALL MATCH N.G")]
+        NG_460 = 1,
+        [Description("4B0 内轮侧 BALL MATCH N.G")]
+        NG_4B0 = 2,
+        [Description("610 HUB侧 BALL CHECK N.G")]
+        NG_610 = 3,
+        [Description("620 HUB侧 GREASE 注入 N.G")]
+        NG_620 = 4,
+        [Description("640 OPEN SEAL 压入 N.G")]
+        NG_640 = 5,
+        [Description("650 OPEN SEAL 扁平度 N.G")]
+        NG_650 = 6,
+        [Description("660 OPEN SEAL GREASE 涂抹 N.G")]
+        NG_660 = 7,
+        [Description("680 COVER RING 压入 N.G")]
+        NG_680 = 8,
+        [Description("690 COVER RING 扁平度 N.G")]
+        NG_690 = 9,
+        [Description("720 内轮侧 BALL CHECK N.G")]
+        NG_720 = 10,
+        [Description("730 内轮侧 GREASE N.G")]
+        NG_730 = 11,
+        [Description("810 INNER RING 压入 N.G")]
+        NG_810 = 12,
+        [Description("作业者 N.G")]
+        NG_Artificial = 99
+            
+    }
+}
---
+++ data-monitor/SL.MLineDataPrecisionTracking.Models/Enum/Assembly_A_LinePassCodeEnum.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SL.MLineDataPrecisionTracking.Models.Enum
+{
+    public enum Assembly_A_LinePassCodeEnum
+    {
+        None = 0,
+        [Description("000 PASS")]
+        PASS_000 = 0b0000000000000001,  // Bit0  1
+        [Description("310 IN PASS")]
+        PASS_IN_310 = 0b0000000000000010,  // Bit1  2
+        [Description("320 OUT PASS")]
+        PASS_OUT_320 = 0b0000000000000100,  // Bit2  4
+        [Description("300 PASS")]
+        PASS_300 = 0b0000000000001000,  // Bit3  8
+        [Description("460 IN PASS")]
+        PASS_IN_460 = 0b0000000000010000,  // Bit4  16
+        [Description("4B0 OUT PASS")]
+        PASS_OUT_4B0 = 0b0000000000100000,  // Bit5  32
+        [Description("500 IN PASS")]
+        PASS_IN_500 = 0b0000000001000000,  // Bit6  64
+        [Description("500A IN PASS")]
+        PASS_IN_500A = 0b0000000010000000,  // Bit7  128
+        [Description("500A OUT PASS")]
+        PASS_OUT_500A = 0b0000000100000000,  // Bit8  256
+        [Description("500B IN PASS")]
+        PASS_IN_500B = 0b0000001000000000,  // Bit9  512
+        [Description("500B OUT PASS")]
+        PASS_OUT_500B = 0b0000010000000000,  // Bit10 1024
+        [Description("610 PASS")]
+        PASS_610 = 0b0000100000000000,  // Bit11 2048
+        [Description("620 PASS")]
+        PASS_620 = 0b0001000000000000,  // Bit12 4096
+        [Description("640 PASS")]
+        PASS_640 = 0b0010000000000000,  // Bit13 8192
+        [Description("650 PASS")]
+        PASS_650 = 0b0100000000000000,  // Bit14 16384
+        [Description("660 PASS")]
+        PASS_660 = 0b1000000000000000,  // Bit15 32768
+        [Description("680 PASS")]
+        PASS_680 = 0b0000000000000001 << 16, // Bit16
+        [Description("690 PASS")]
+        PASS_690 = 0b0000000000000001 << 17, // Bit17
+        [Description("710 PASS")]
+        PASS_710 = 0b0000000000000001 << 18, // Bit18
+        [Description("720 PASS")]
+        PASS_720 = 0b0000000000000001 << 19, // Bit19
+        [Description("730 PASS")]
+        PASS_730 = 0b0000000000000001 << 20, // Bit20
+        [Description("810 PASS")]
+        PASS_810 = 0b0000000000000001 << 21  // Bit21
+    }
+}
