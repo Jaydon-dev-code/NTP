55df309ee6ea877eaf7829744cd65997235fcdab
C2300040802 <C2300040802@C2300040802>
2026-07-13 17:16:37 +0800

    fix：修复软件启动类型错误的问题

--- data-monitor/SL.MLineDataPrecisionTracking.Models/Enum/Assembly_A_LineNgCodeEnum.cs
+++ data-monitor/SL.MLineDataPrecisionTracking.Models/Enum/Assembly_A_LineNgCodeEnum.cs
@@ -40,6 +40,10 @@ namespace SL.MLineDataPrecisionTracking.Models.Enum
         NG_730 = 11,
         [Description("810 INNER RING 压入 N.G")]
         NG_810 = 12,
+        [Description("内法兰扫码 N.G")]
+        NG_内法兰_扫码 = 13,
+        [Description("外法兰扫码 N.G")]
+        NG_外法兰_扫码 = 14,
         [Description("作业者 N.G")]
         NG_Artificial = 99
             
