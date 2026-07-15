dd3bd682a0cf87cd05aa788c7bf8a00432c708d9
C2300040802 <C2300040802@C2300040802>
2026-07-15 16:03:57 +0800

    feat:五菱精追前端架构提交

---
+++ data-monitor/SL.MLineDataPrecisionTracking.Client/View/Control/Factory4Workshop4_10Line.xaml.cs
@@ -0,0 +1,32 @@
+﻿using SL.MLineDataPrecisionTracking.Client.Common;
+using SL.MLineDataPrecisionTracking.Client.ViewModel.Control;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SL.MLineDataPrecisionTracking.Client.View.Control
+{
+    /// <summary>
+    /// Factory4Workshop4_10Line.xaml 的交互逻辑
+    /// </summary>
+    [ViewLinkServerInfoAttribute(serverNames: new[] { "", "" }, header: "装配线数据查询", icon: "Resources\\Image\\ProductionRecord.png", MatchMode = ViewMatchMode.Any)]
+    public partial class Factory4Workshop4_10Line : UserControl
+    {
+        public Factory4Workshop4_10Line(Factory4Workshop4_10LineViewModel factory4Workshop4_10LineViewModel)
+        {
+            this.DataContext = factory4Workshop4_10LineViewModel;
+            InitializeComponent();
+        }
+    }
+}
---
+++ data-monitor/SL.MLineDataPrecisionTracking.Client/ViewModel/Control/Factory4Workshop4_10LineViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+using CommunityToolkit.Mvvm.ComponentModel;
+using SL.MLineDataPrecisionTracking.Client.Http;
+using SL.MLineDataPrecisionTracking.Models.Domain;
+
+namespace SL.MLineDataPrecisionTracking.Client.ViewModel.Control
+{
+    public class Factory4Workshop4_10LineViewModel : ObservableObject
+    {
+        private bool _isOnlieClearance;
+        public bool IsOnlieClearance
+        {
+            get => _isOnlieClearance;
+            set => SetProperty(ref _isOnlieClearance, value);
+        }
+
+        private bool _isOnlieRivetAndCrack;
+        public bool IsOnlieRivetAndCrack
+        {
+            get => _isOnlieRivetAndCrack;
+            set => SetProperty(ref _isOnlieRivetAndCrack, value);
+        }
+
+        private bool _isOnlieVib;
+        public bool IsOnlieVib
+        {
+            get => _isOnlieVib;
+            set => SetProperty(ref _isOnlieVib, value);
+        }
+
+        private bool _isOnlieABS;
+        public bool IsOnlieABS
+        {
+            get => _isOnlieABS;
+            set => SetProperty(ref _isOnlieABS, value);
+        }
+
+        public Factory4Workshop4_10LineViewModel(HubClien hubClien)
+        {
+            UpValue(hubClien);
+        }
+
+        void UpValue(HubClien hubClien)
+        {
+            // 判断是否在线
+            hubClien.Start<bool>(nameof(IsOnlieClearance), x => IsOnlieClearance = x);
+            hubClien.Start<bool>(nameof(IsOnlieRivetAndCrack), x => IsOnlieRivetAndCrack = x);
+            hubClien.Start<bool>(nameof(IsOnlieVib), x => IsOnlieVib = x);
+            hubClien.Start<bool>(nameof(IsOnlieABS), x => IsOnlieABS = x);
+            //接收后台数据
+            //hubClien.Start<bool>(nameof(IsOnlieClearance), x => IsOnlieClearance = x);
+            //hubClien.Start<bool>(nameof(IsOnlieRivetAndCrack), x => IsOnlieRivetAndCrack = x);
+            //hubClien.Start<bool>(nameof(IsOnlieVib), x => IsOnlieVib = x);
+            //hubClien.Start<bool>(nameof(IsOnlieABS), x => IsOnlieABS = x);
+        }
+    }
+}
