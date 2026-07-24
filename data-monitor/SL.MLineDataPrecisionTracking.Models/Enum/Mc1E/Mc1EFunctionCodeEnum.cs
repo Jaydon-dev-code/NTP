167c098ad12ff32293de6218bfec0fa6a3b735e6
C2300040802 <C2300040802@C2300040802>
2026-07-24 09:25:27 +0800

    feat：添加ec 1e 功能码枚举

---
+++ data-monitor/SL.MLineDataPrecisionTracking.Models/Enum/Mc1E/Mc1EFunctionCodeEnum.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SL.MLineDataPrecisionTracking.Models.Enum.Mc1E
+{
+    public enum Mc1EFunctionCodeEnum : byte
+    {
+        /// <summary>
+        /// 批量位读取
+        /// </summary>
+        BatchBitRead = 0x00,
+
+        /// <summary>
+        /// 批量字读取
+        /// </summary>
+        BatchWordRead = 0x01,
+
+        /// <summary>
+        /// 批量位写入
+        /// </summary>
+        BatchBitWrite = 0x02,
+
+        /// <summary>
+        /// 批量字写入
+        /// </summary>
+        BatchWordWrite = 0x03,
+
+        /// <summary>
+        /// 随机位写入
+        /// </summary>
+        RandomBitWrite = 0x04,
+
+        /// <summary>
+        /// 随机字写入
+        /// </summary>
+        RandomWordWrite = 0x05,
+    }
+}
