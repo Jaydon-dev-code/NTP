d9887cd1052ebbde58733e9020c57c4e44e8e51e
C2300040802 <C2300040802@C2300040802>
2026-07-24 09:26:06 +0800

    feat：添加游隙dto模型

--- data-monitor/SL.MLineDataPrecisionTracking.Models/Dtos/Factory4Workshop4_10Line/Factory4Workshop4_10Line_ClearanceDto.cs
+++ data-monitor/SL.MLineDataPrecisionTracking.Models/Dtos/Factory4Workshop4_10Line/Factory4Workshop4_10Line_ClearanceDto.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SL.MLineDataPrecisionTracking.Models.Enum;
 using SqlSugar;
+using SqlSugar.DbConvert;
 
 namespace SL.MLineDataPrecisionTracking.Models.Dtos.Factory4Workshop4_10Line
 {
@@ -13,20 +15,28 @@ namespace SL.MLineDataPrecisionTracking.Models.Dtos.Factory4Workshop4_10Line
         public DateTime RecordTime { get; set; }
         public string SN { get; set; }
 
+        [Description("游隙检测结果")]
         public ResultEnum ClearanceResult { get; set; }
 
+        [Description("正间隙")]
         public string PositiveGap { get; set; }
 
+        [Description("下负荷")]
         public string LowerLoad { get; set; }
 
+        [Description("上负荷")]
         public string UpperLoad { get; set; }
 
+        [Description("偏移量")]
         public string Offset { get; set; }
 
+        [Description("压入前")]
         public string BeforePressIn { get; set; }
 
+        [Description("压入后")]
         public string AfterPressIn { get; set; }
 
+        [Description("间隙")]
         public string Gap { get; set; }
     }
 }
