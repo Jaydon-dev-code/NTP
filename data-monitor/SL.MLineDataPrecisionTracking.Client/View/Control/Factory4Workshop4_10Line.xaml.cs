d24df9cf9bd3bb05ec0ff4d0918d20673771184c
C2300040802 <C2300040802@C2300040802>
2026-07-29 08:01:29 +0800

    feat:添加关联服务

--- data-monitor/SL.MLineDataPrecisionTracking.Client/View/Control/Factory4Workshop4_10Line.xaml.cs
+++ data-monitor/SL.MLineDataPrecisionTracking.Client/View/Control/Factory4Workshop4_10Line.xaml.cs
@@ -1,6 +1,4 @@
-﻿using SL.MLineDataPrecisionTracking.Client.Common;
-using SL.MLineDataPrecisionTracking.Client.ViewModel.Control;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,16 +12,31 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using SL.MLineDataPrecisionTracking.Client.Common;
+using SL.MLineDataPrecisionTracking.Client.ViewModel.Control;
 
 namespace SL.MLineDataPrecisionTracking.Client.View.Control
 {
     /// <summary>
     /// Factory4Workshop4_10Line.xaml 的交互逻辑
     /// </summary>
-    [ViewLinkServerInfoAttribute(serverNames: new[] { "", "" }, header: "装配线数据查询", icon: "Resources\\Image\\ProductionRecord.png", MatchMode = ViewMatchMode.Any)]
+    [ViewLinkServerInfoAttribute(
+        serverNames: new[]
+        {
+            "Factory4Workshop4_10Line_ABS",
+            "Factory4Workshop4_10Line_Clearance",
+            "Factory4Workshop4_10Line_RivetAndCrack",
+            "Factory4Workshop4_10Line_Vib",
+        },
+        header: "装配线数据查询",
+        icon: "Resources\\Image\\ProductionRecord.png",
+        MatchMode = ViewMatchMode.Any
+    )]
     public partial class Factory4Workshop4_10Line : UserControl
     {
-        public Factory4Workshop4_10Line(Factory4Workshop4_10LineViewModel factory4Workshop4_10LineViewModel)
+        public Factory4Workshop4_10Line(
+            Factory4Workshop4_10LineViewModel factory4Workshop4_10LineViewModel
+        )
         {
             this.DataContext = factory4Workshop4_10LineViewModel;
             InitializeComponent();
