f3870e6868bee673cfed1405f6f8918e117720ab
C2300040802 <C2300040802@C2300040802>
2026-08-10 15:19:31 +0800

    feat:版本提升为 1.1.2.2

--- data-monitor/SL.MLineDataPrecisionTracking.Client/Properties/AssemblyInfo.cs
+++ data-monitor/SL.MLineDataPrecisionTracking.Client/Properties/AssemblyInfo.cs
@@ -48,5 +48,5 @@ using System.Windows;
 //      生成号
 //      修订号
 //
-[assembly: AssemblyVersion("1.1.2.1")]
-[assembly: AssemblyFileVersion("1.1.2.1")]
+[assembly: AssemblyVersion("1.1.2.2")]
+[assembly: AssemblyFileVersion("1.1.2.2")]
